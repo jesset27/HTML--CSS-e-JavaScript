5fe513562c17f29c92693181017d489d0b01135d
Jessé Willian <jessewillian0@gmail.com>
2021-11-30 10:23:50 -0300

    exe1.0

--- exe1/script.js
+++ exe1/script.js
@@ -1,6 +1,8 @@
 function carregar(){
     var msg = window.document.getElementById("msg")
+
     var img = window.document.getElementById("imagem")
+    
     var bom = window.document.getElementById("bom")
     var data = new Date()
     var hora = data.getHours()
