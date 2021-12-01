32894fe1efb5e6210dc910a7e4656add07f11938
Jessé Willian <jessewillian0@gmail.com>
2021-12-01 08:40:25 -0300

    2.2

--- exe2/script.js
+++ exe2/script.js
@@ -2,8 +2,8 @@ function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById("txtano")
-    var res = document.getElementById("res")
-    if (fano.ariaValueMax.length == 0 || Number(fano.value) > ano){
+    var res = document.querySelector("div#res")
+    if (fano.value.length == 0 || Number(fano.value) > ano){
         window.alert("(ERRO) Verifique os dados e tente novamente!")
     }
     else {
