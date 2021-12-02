29279d3a5d050f451763434b59e00c8776480209
Jessé Willian <jessewillian0@gmail.com>
2021-12-02 18:00:39 -0300

    2.3

--- exe2/script.js
+++ exe2/script.js
@@ -9,6 +9,44 @@ function verificar(){
     else {
         var fsex = document.getElementsByName("radsex")
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        var genero = ""
+        var img = document.createElement("img")
+        img.setAttribute("id", "foto")
+        if (fsex[0].checked) {
+            genero = "Homem"
+            if (idade >= 0 && idade < 10) {
+                //crianca
+                img.setAttribute("src", "./imagens/bebe-homem.png")
+            }
+            else if (idade >= 10 && idade < 21) {
+                //jovem
+                img.setAttribute("src", "./imagens/jovem-homem.png")
+            }
+            else if (idade < 50) {
+                // adulto
+                img.setAttribute("src", "./imagens/adulto-homem.png")
+            }
+            else {
+                //idoso
+                img.setAttribute("src", "./imagens/idoso-homem.png")
+            }
+        }
+        else if (fsex[1].checked) {
+            genero = "Mulher"
+            if (idade >= 0 && idade < 10) {
+                //crianca
+            }
+            else if (idade >= 10 && idade < 21) {
+                //jovem
+            }
+            else if (idade < 50) {
+                // adulto
+            }
+            else {
+                //idoso
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
